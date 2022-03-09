4b5e31f65c38f14cf61fd1bff803bf300e123c64
Abhishek Udnur <76278098+AbhishekUdnur@users.noreply.github.com>
2022-03-09 14:47:13 +0530

    Update converter.py

--- converter.py
+++ converter.py
@@ -4,26 +4,25 @@ from bottle import redirect, static_file
 import os
 from PIL import Image
 
-
 converter = Bottle()
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 def get_save_path_with_format(filename, extension):
-	if not extension.startswith('.'):
-		extension = '.' + extension
-	return ''.join(_ for _ in [BASE_DIR, '/media/', filename, extension])
+    if not extension.startswith('.'):
+        extension = '.' + extension
+    return ''.join(_ for _ in [BASE_DIR, '/media/', filename, extension])
 
 
 @converter.route('/', method='GET')
 def get_form():
     return '''
-		<head>
-			<link
-			href="https://fonts.googleapis.com/css?family=Raleway:400,600&display=swap"
-			rel="stylesheet">
-		</head>
-		<body>
+        <head>
+	        <link 
+	        href="https://fonts.googleapis.com/css?family=Raleway:400,600&display=swap"
+	        rel="stylesheet">
+	    </head>
+	    <body>
 	    	<div class="wrapper">
 				<h2><strong>IMAGE_FORMAT_CONVERTER</strong></h2>
 				<form action="/"
@@ -37,7 +36,6 @@ def get_form():
 					<select name="output_file" id="out">
 					<option value="gif">GIF</option>
 					<option value="png">PNG</option>
-					<option value="jpeg">JPEG</option>
 					<option value="webp">WEBP</option>
 					<option value="jpg">jpg</option>
 					</select>
@@ -92,60 +90,47 @@ def get_form():
 
 @converter.route('/', method='POST')
 def do_upload():
-	quality = 100
-	optional_name = request.forms.get('optional_name')
-	uploaded = request.files.get('upload')
-	out_file_ext = request.forms.get('output_file')
-	print(out_file_ext)
-	name, ext = os.path.splitext(uploaded.filename)
-	if ext.lower() not in ('.png', '.jpg', '.jpeg', '.webp', '.gif'):
-	    return 'File extension not allowed.'
+    quality = 100
+    optional_name = request.forms.get('optional_name')
+    uploaded = request.files.get('upload')
+    out_file_ext = request.forms.get('output_file')
+    print(out_file_ext)
+    name, ext = os.path.splitext(uploaded.filename)
+    if ext.lower() not in ('.png', '.jpg', '.jpeg', '.webp', '.gif'):
+        return 'File extension not allowed.'
 
-	if optional_name != '':
-	    name = optional_name
+    if optional_name != '':
+        name = optional_name
 
-	save_path = get_save_path_with_format(name, ext)
+    save_path = get_save_path_with_format(name, ext)
 
-	try:
-	    uploaded.save(save_path + ext)
-	except IOError:
-	    os.remove(save_path + ext)
-	    uploaded.save(save_path + ext)
-	
-	try:
-		
-		if ext.lower() not in ('.png', '.jpg', '.webp', '.gif'):
-			
-			try:
-				pass
-				
-			except OSError:
-				im = Image.open(save_path + ext)
-				e = im.convert('RGB')
-				output_path = get_save_path_with_format(name, '.'+out_file_ext)
-				e.save(output_path.split('.')[0] +'.' + 'jpeg')
-				os.remove(save_path + ext)
-				redirect("/converted/%s" % name + '.'+out_file_ext)
-		else:
-			im = Image.open(save_path + ext)
-			output_path = get_save_path_with_format(name, '.'+out_file_ext)
-			im.save(output_path, out_file_ext, quality=quality)
-			os.remove(save_path + ext)
-			redirect("/converted/%s" % name + '.'+out_file_ext)
-	
-	except KeyError:
-		
-		im = Image.open(save_path + ext)
-		e = im.convert('RGB')
-		output_path = get_save_path_with_format(name, '.'+out_file_ext)
-		e.save(output_path.split('.')[0] +'.' + 'jpg')
-		os.remove(save_path + ext)
-		redirect("/converted/%s" % name + '.'+out_file_ext)
+    try:
+        uploaded.save(save_path + ext)
+    except IOError:
+        os.remove(save_path + ext)
+        uploaded.save(save_path + ext)
+
+    try:
+        im = Image.open(save_path + ext)
+        output_path = get_save_path_with_format(name, '.' + out_file_ext)
+        im.save(output_path, out_file_ext, quality=quality)
+        os.remove(save_path + ext)
+        redirect("/converted/%s" % name + '.' + out_file_ext)
+
+    except KeyError:
+        im = Image.open(save_path + ext)
+        e = im.convert('RGB')
+        output_path = get_save_path_with_format(name, '.' + out_file_ext)
+        e.save(output_path.split('.')[0] + '.' + 'jpg')
+        os.remove(save_path + ext)
+        redirect("/converted/%s" % name + '.' + out_file_ext)
+
 
 @converter.route('/download/<filename:path>')
 def send_image(filename):
-	print('filename - ',filename)
-	return static_file(filename,root='/', download=filename)
+    print('filename - ', filename)
+    return static_file(filename, root='/', download=filename)
+
 
 @converter.route('/converted/<filename:path>')
 def updloaded(filename):
@@ -156,4 +141,3 @@ def updloaded(filename):
 
 
 run(converter, host='localhost', port='8080', debug=True)
-
